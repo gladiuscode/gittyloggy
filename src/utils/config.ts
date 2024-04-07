6993fc7a0889b9ba100539d0737cc0a3f5fac3e3
gladiuscode <gladiusservices@protonmail.com>
2024-04-07 23:57:38 +0200

    feat: setup prettier and fix files

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,4 @@
-import {readFile} from "fs/promises";
+import {readFile} from 'fs/promises';
 
 export interface Config {
   PEOPLE_TAGS: string;
@@ -14,25 +14,30 @@ const openConfig = async () => {
   } catch (error) {
     return;
   }
-}
+};
 
 const ConfigUtils = {
-  build: async function() {
+  build: async function () {
     const rawConfig = await openConfig();
     if (!rawConfig) {
-      throw new Error('[GittyLoggy] - Please check your .gitty.config.json file');
+      throw new Error(
+        '[GittyLoggy] - Please check your .gitty.config.json file',
+      );
     }
     const config = JSON.parse(rawConfig);
 
     if (!config.VERSION) throw new Error('VERSION config property is missing');
-    if (!config.PIPELINE_URL) throw new Error('PIPELINE_URL config property is missing');
-    if (!config.PEOPLE_TAGS) throw new Error('PEOPLE_TAGS config property is missing');
+    if (!config.PIPELINE_URL)
+      throw new Error('PIPELINE_URL config property is missing');
+    if (!config.PEOPLE_TAGS)
+      throw new Error('PEOPLE_TAGS config property is missing');
     if (!config.TAG) throw new Error('TAG config property is missing');
-    if (!config.ONLY_MERGED_PRS) throw new Error('ONLY_MERGED_PRS config property is missing');
+    if (!config.ONLY_MERGED_PRS)
+      throw new Error('ONLY_MERGED_PRS config property is missing');
 
     return config as Config;
   },
-}
+};
 
 Object.freeze(ConfigUtils);
 
